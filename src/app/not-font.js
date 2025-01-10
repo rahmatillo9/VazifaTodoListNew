709ef507e44860c802a800ca3dd44ce4e4771c1a
PPES571 <rsayfiddinov571@mail.ru>
2025-01-10 14:58:52 +0500

    D:/Git/

--- src/app/not-font.js
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>404 - Sahifa topilmadi</h1>
-        <p>Bosh sahifaga qaytish uchun <a href="/">bu yerni bosing</a>.</p>
-      </div>
-    );
-  }
-  
